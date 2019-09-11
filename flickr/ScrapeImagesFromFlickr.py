0101e2fe26cb4b5daba966d00b2acb08e5819828
angwhen <angelmwen@gmail.com>
2019-09-11 14:51:20 -0400

    scraping images and saving locally from flickr

---
+++ flickr/ScrapeImagesFromFlickr.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import csv
+from os import walk
+import urllib.request
+
+# save the images
+# and also a dataframe from url to image name
+
+data = None
+for keyword in open("data/keywords.txt"):
+    keyword = keyword.strip()
+    if data is None:
+        data = pd.read_csv("data/%s_urls.csv"%keyword)
+    else:
+        data = data.append(pd.read_csv("data/%s_urls.csv"%keyword))
+
+
+images_and_urls = []
+count = 0
+for i,row in data.iterrows():
+    print ("%s %d"%(url,count))
+    url = row['url']
+    try:
+        fname = "data/images/%d.jpg"%count
+        urllib.request.urlretrieve(url,fname)
+        images_and_urls.append([url,fname])
+        count +=1
+    except:
+        count +=0
+urls.to_csv("data/images/url_to_image_file.csv")
