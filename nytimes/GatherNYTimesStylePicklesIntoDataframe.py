7182994673e283ff9842c0a1bf1d66fe52edc98a
angwhen <angelmwen@gmail.com>
2019-09-13 20:52:11 -0400

    starting frame for gathering pickles into df

--- nytimes/GatherNYTimesStylePicklesIntoDataframe.py
+++ nytimes/GatherNYTimesStylePicklesIntoDataframe.py
@@ -2,3 +2,11 @@ import time
 import json
 import pickle
 import pandas
+
+all_years = []
+for year in range(2019,2020): #start 1852
+    curr = pickle.load(open("data/nytimes_style_articles/year_%d.p"%year,"rb"))
+    all_years.extend(curr)
+
+df=pd.DataFrame(all_years)
+df.columns = ["year","month","text","matched_keywords"]
