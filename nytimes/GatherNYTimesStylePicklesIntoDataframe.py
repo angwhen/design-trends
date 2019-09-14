57b33b40f1a188525931b1068be3ad9dc4fffa1c
angwhen <angelmwen@gmail.com>
2019-09-14 12:41:51 -0400

    extracting cols to dataframe from nytimes style data

--- nytimes/GatherNYTimesStylePicklesIntoDataframe.py
+++ nytimes/GatherNYTimesStylePicklesIntoDataframe.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 all_years = []
 for year in range(1852,2020): #start 1852
+    print (year)
     curr = pickle.load(open("data/nytimes_style_articles/year_%d.p"%year,"rb"))
     all_years.extend(curr)
 
