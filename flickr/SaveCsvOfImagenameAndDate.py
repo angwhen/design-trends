29e5bb9cf7da14ee410e9fbea96dcae54aa61ee3
angwhen <angelmwen@gmail.com>
2019-09-14 21:47:17 -0400

    some prints

--- flickr/SaveCsvOfImagenameAndDate.py
+++ flickr/SaveCsvOfImagenameAndDate.py
@@ -11,13 +11,13 @@ for keyword in keywords_set:
         df =  pd.read_csv("data/%s_urls.csv"%keyword)
     else:
         df = df.append(pd.read_csv("data/%s_urls.csv"%keyword))
-
 df = df.drop_duplicates(subset=['url','title'], keep="first")
+print ("unique url+title pairs fromflickr: %d" %len(df))
 
 url_to_image_file_df = pd.read_csv("data/images/url_to_image_file.csv")
 
 res_df = pd.merge(df, url_to_image_file_df, on='url', how='inner')
-
 res_df = res_df[["url","title","year","file_name"]]
 
 res_df.to_csv("data/url_title_and_file_data.csv")
+print ("final df length: %d" %len(res_df))
