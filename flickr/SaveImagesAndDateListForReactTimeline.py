9ac8689ef8a0ec155ee86c18e960773bad51908c
angwhen <angelmwen@gmail.com>
2019-09-21 20:11:22 -0400

    updating images code for react slideshow to use flickr url while waiting on aws stuff

--- flickr/SaveImagesAndDateListForReactTimeline.py
+++ flickr/SaveImagesAndDateListForReactTimeline.py
@@ -1,30 +1,27 @@
 import pandas as pd
 
 
-df =  pd.read_csv("data/url_title_and_file_data.csv")
-my_list = df[["file_name","year"]].values.tolist()
-my_list.sort(key=lambda x: x[1])
+def get_images_code_for_react():
+    df =  pd.read_csv("data/url_title_and_file_data.csv")
+    my_list = df[["url","year"]].values.tolist()
+    my_list.sort(key=lambda x: x[1])
 
-#TODO: want to also filter out images that are not clothes/do not have people
+    #TODO: want to also filter out images that are not clothes/do not have people
 
 
-my_str = ""
-for im in my_list:
-    fname_specific = im[0].split("/")[-1]
+    my_str = ""
+    my_str += "  images: [\n"
+    for im in my_list[:-2]:
+        fname_specific = im[0].split("/")[-1]
+        fname_num = fname_specific.split(".")[0]
+        my_str += "[im%s,'%s'],\n"%(fname_num,im[1])
+    fname_specific = my_list[-1][0].split("/")[-1]
     fname_num = fname_specific.split(".")[0]
-    my_str += "import im%s from '../flickr_images/default_images/smaller_images/%s';\n"%(fname_num,fname_specific)
+    my_str += "[im%s,'%s']\n"%(fname_num , my_list[-1][1])
+    my_str += "],"
 
-my_str += "\n"
-my_str += "  images: [\n"
-for im in my_list[:-2]:
-    fname_specific = im[0].split("/")[-1]
-    fname_num = fname_specific.split(".")[0]
-    my_str += "[im%s,'%s'],\n"%(fname_num,im[1])
-fname_specific = my_list[-1][0].split("/")[-1]
-fname_num = fname_specific.split(".")[0]
-my_str += "[im%s,'%s']\n"%(fname_num , my_list[-1][1])
-my_str += "],"
+    text_file = open("data/react_default_images_timeline_code_lists_and_imports.txt", "w")
+    text_file.write(my_str)
+    text_file.close()
 
-text_file = open("data/react_default_images_timeline_code_lists_and_imports.txt", "w")
-text_file.write(my_str)
-text_file.close()
+get_images_code_for_react()
