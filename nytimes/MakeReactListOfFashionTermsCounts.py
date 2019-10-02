637f3caa972322984ef83a8117fdbc8f0489880a
angwhen <angelmwen@gmail.com>
2019-10-02 12:03:59 -0400

    mayb monthly fashion

--- nytimes/MakeReactListOfFashionTermsCounts.py
+++ nytimes/MakeReactListOfFashionTermsCounts.py
@@ -9,9 +9,8 @@ year_keywords_df = df.groupby('year')['matched_keywords'].apply(list).reset_inde
 month_keywords_df = df.groupby(['year','month'])['matched_keywords'].apply(list).reset_index()
 year_keywords_list = year_keywords_df[['year',"matched_keywords"]].values.tolist()
 year_keywords_list.sort(key=lambda x: x[0])
-"""month_keywords_list = month_keywords_df[['year','month',"matched_keywords"]].values.tolist()
-month_keywords_list.sort(key=lambda x: x[1])
-month_keywords_list.sort(key=lambda x: x[0])"""
+#month_keywords_list = month_keywords_df[['month',"matched_keywords"]].values.tolist()
+#month_keywords_list.sort(key=lambda x: x[0])
 
 flatten = lambda l: [item for sublist in l for item in sublist]
 
@@ -28,14 +27,13 @@ for row in year_keywords_list:
     cnts = [[tup[0],tup[1]/total_word_counts_dict[tup[0]]] for tup in Counter(keywords).most_common(50)]
     my_str += "[%s, %s],\n"%(year,cnts)
 my_str = my_str[:-2]+"],\n"
-
-"""my_str += "monthly_fashion_terms:["
+"""
+my_str += "monthly_fashion_terms:["
 for row in month_keywords_list:
-    year = row[0]
-    month = row[1]
-    keywords =flatten([el[2:-2].split("', '" ) for el in row[2]])
-    cnts =[[tup[0],tup[1]] for tup in Counter(keywords).most_common(20)]
-    my_str += "[%s, %s, %s],\n"%(year,month,cnts)
+    month = row[0]
+    keywords = flatten([el[2:-2].split("', '" ) for el in row[1] if el != "Style"])
+    cnts = [[tup[0],tup[1]/total_word_counts_dict[tup[0]]] for tup in Counter(keywords).most_common(50)]
+    my_str += "[%s, %s],\n"%(month,cnts)
 my_str = my_str[:-2]+"],\n"
 """
 
