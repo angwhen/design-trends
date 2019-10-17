6f5222455e3b854177c1af6361c58624ebb06073
angwhen <angelmwen@gmail.com>
2019-10-16 22:11:33 -0400

    dist from vector center

--- fashion_terms/GenMoreFashionTermsFromPretrainedWordVecs.py
+++ fashion_terms/GenMoreFashionTermsFromPretrainedWordVecs.py
@@ -1,5 +1,7 @@
 import spacy
 import pickle
+import numpy as np
+from scipy.spatial import distance
 nlp = spacy.load('en_core_web_md')
 
 def idk():
@@ -13,14 +15,23 @@ def idk():
 
 
 def get_distance_from_center_of_unigram_terms_in_nytimes_style_dataset():
-    d = pickle.load(open("data/nytimes_style_articles/style_related_words_cooccurence_matrix.p","rb"))
+    d = pickle.load(open("../nytimes/data/nytimes_style_articles/style_related_words_cooccurence_matrix.p","rb"))
     words = d[0]
     style_words_to_vec_dict = {}
     for w in words:
         if len(w.split()) == 1:
             doc = nlp(w)
             style_words_to_vec_dict[w] = doc[0].vector
+    vecs_list = np.array(list(style_words_to_vec_dict.values()))
+    mean_vec = np.mean(vecs_list,axis=0)
 
+    style_words_to_dist_from_center_dict = {}
+    for w in style_words_to_vec_dict:
+        style_words_to_dist_from_center_dict[w] = distance.euclidean(style_words_to_vec_dict[w],mean_vec)
+
+    pickle.dump(style_words_to_dist_from_center_dict,open("data/my_data/terms_to_distance_from_vector_center_dict.p","wb"))
+
+get_distance_from_center_of_unigram_terms_in_nytimes_style_dataset()
 """import io
 
 def load_vectors(fname):
--- fashion_terms/GenReactCodeForDiffFashionabilityMetrics.py
+++ fashion_terms/GenReactCodeForDiffFashionabilityMetrics.py
@@ -3,10 +3,12 @@ import pickle
 nytimes_term_to_deg_dict = pickle.load(open("data/my_data/nytimes_term_to_deg_dict.p","rb"))
 nytimes_term_to_normalized_deg_dict = pickle.load(open("data/my_data/nytimes_term_to_normalized_deg_dict.p","rb"))
 nytimes_term_to_eig_centrality_dict = pickle.load(open("data/my_data/nytimes_term_to_eig_centrality_dict.p","rb"))
+terms_to_distance_from_vector_center_dict = pickle.load(open("data/my_data/terms_to_distance_from_vector_center_dict.p","rb"))
 
 all_terms = list(nytimes_term_to_deg_dict.keys())
 all_terms.extend( list(nytimes_term_to_normalized_deg_dict.keys()))
 all_terms.extend( list(nytimes_term_to_eig_centrality_dict.keys()))
+all_terms.extend( list(terms_to_distance_from_vector_center_dict.keys()))
 all_terms = list(set(all_terms))
 my_str = " ["
 #term: "Raymond",
@@ -21,6 +23,8 @@ for t in all_terms:
         my_str += "score2: %f, "%nytimes_term_to_normalized_deg_dict[t]
     if t in nytimes_term_to_eig_centrality_dict:
         my_str += "score3: %f, "%nytimes_term_to_eig_centrality_dict[t]
+    if t in terms_to_distance_from_vector_center_dict:
+        my_str += "score4: %f, "%terms_to_distance_from_vector_center_dict[t]
     my_str = my_str[:-2] + "},\n"
 my_str = my_str[:-2] + "];"
 
