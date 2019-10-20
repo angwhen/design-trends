cc525554721a1f61fcc7fc06961be8bb7f582866
angwhen <angelmwen@gmail.com>
2019-10-20 18:59:40 -0400

    edit to chunking

--- nytimes/OrganizeNYTimesStyleDf.py
+++ nytimes/OrganizeNYTimesStyleDf.py
@@ -144,15 +144,18 @@ def get_hand_curated_style_terms_articles_df():
 def add_tokenage_to_parsed(starter = "parsed_only_articles_df.csv"):
     print ("Starting from parsed only data")
     fin_df = pd.DataFrame(columns=['year','month','main_parts_text','type_of_material','web_url','headline','word_count','abstract','snippet','lead_paragraph','keywords','section_name','subsection_name','pub_date','nouns_in_main_parts','adjectives_in_main_parts','matched_keywords'])
+    count = 0
     for chunk in pd.read_csv("%s/data/nytimes_style_articles/%s"%(DATA_PATH,starter), chunksize=50000):
+        print ("starting with chunk %d"%count)
         df = chunk
         df["main_parts_text"] = df.apply(get_main_parts, axis = 1)
         df["noun_phrases_in_main_parts"] = df.apply(get_noun_phrases, axis = 1) # headline, abstract, snipper, lead paragraph
         df["nouns_in_main_parts"] = df.apply(get_nouns, axis = 1)
         df["adjectives_in_main_parts"] = df.apply(get_adjectives, axis = 1)
-        pd.concat([fin_df,df], ignore_index=True)
+        fin_df = pd.concat([fin_df,df], ignore_index=True)
         del df
         gc.collect()
+        count +=1
     """
     df = pd.read_csv("%s/data/nytimes_style_articles/%s"%(DATA_PATH,starter))
     df["main_parts_text"] = df.apply(get_main_parts, axis = 1)
