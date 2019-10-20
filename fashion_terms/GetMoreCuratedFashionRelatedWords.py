55c7cc9b7105e9eaadff4943b97e58117ebded57
angwhen <angelmwen@gmail.com>
2019-10-20 18:45:17 -0400

    fashion words curating starter

--- fashion_terms/GetMoreCuratedFashionRelatedWords.py
+++ fashion_terms/GetMoreCuratedFashionRelatedWords.py
@@ -5,6 +5,8 @@ import json
 import pickle
 from nltk.tag import pos_tag
 import pandas as pd
+import csv
+
 
 def make_style_words_df():
     style_related_words = set([])
@@ -39,25 +41,58 @@ def make_style_words_df():
                 style_related_words_unigram.add(w)
         first = False
 
-    all_style_related_words = list(style_related_words_unigram.union(style_related_words))
+    human_labels_dict = {}
+    with open('data/my_data/human_labelled_fashion_terms.csv', 'rb') as f:
+        reader = csv.reader(f)
+        human_labels = list(reader)
+    for w in human_labels:
+        if len(w[1]) != 0:
+            human_labels_dict[w[0]] = int(w[1])
 
     all_words = []
     for word in style_related_words_unigram:
         p = pos_tag(word.split())
+
+        label = 1
         if "homepage" in word.lower() or ".com" in word.lower():
-            all_words.append([word,1,p,0])
-        else:
-            all_words.append([word,1,p,1])
+            label = 0
+        if word in human_labels_dict:
+            label = human_labels_dict[word]
+
+        proper = 0
+        if len(p) >=2:
+            all_proper = True
+            for el in p:
+                if el[1] != "NNP":
+                    all_proper = False
+            if all_proper:
+                proper = 1
+
+        all_words.append([word,1,label,proper,p])
     for word in style_related_words:
         p = pos_tag(word.split())
-        if "homepage" in word.lower() or ".com" in word.lower():
-            all_words.append([word,0,p,0])
-        else:
-            all_words.append([word,0,p,1]) # setting all human edited labels to 1, unless otherwise edited
 
-    # columns of the dataframe should be: WORD, Unigram or not, POS tagged
+        label = 1
+        if "homepage" in word.lower() or ".com" in word.lower():
+            label = 0
+        if word in human_labels_dict:
+            label = human_labels_dict[word]
+
+        proper = 0
+        if len(p) >=2:
+            all_proper = True
+            for el in p:
+                if el[1] != "NNP":
+                    all_proper = False
+            if all_proper:
+                proper = 1
+
+        all_words.append([word,0,label,proper,p]) # default is 1 unless other reason
+
+    # columns of the dataframe should be: WORD, Unigram or not, POS tagged, my label
+    # my label is 0 for do not use, 1 for use, 2 for depends on the context
     df=pd.DataFrame(all_words)
-    df.columns = ["word","unigram","pos_tags","human_edited_label"]
+    df.columns = ["word","unigram","human_edited_label","proper","pos_tags"]
     df.to_csv("data/my_data/fashion_terms.csv")
 
 make_style_words_df()
