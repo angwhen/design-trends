d43c6bb10d1128cc64ea8621f5b61453e04c3bea
angwhen <angelmwen@gmail.com>
2019-10-29 20:23:04 -0400

    default message

--- nytimes/LocationsAnalysis.py
+++ nytimes/LocationsAnalysis.py
@@ -25,36 +25,36 @@ except:
 # make react word cloud for this
 def make_map_from_years_to_locations():
     df = pd.read_csv("%s/data/nytimes_style_articles/locationed_curated_tokenaged_parsed_only_articles_df.csv"%DATA_PATH)
-    date_and_fashion_terms = df[["year","month","curated_matched_keywords"]]
-    year_keywords_df = df.groupby('year')['curated_matched_keywords'].apply(list).reset_index()
-    month_keywords_df = df.groupby(['year','month'])['curated_matched_keywords'].apply(list).reset_index()
-    year_keywords_list = year_keywords_df[['year',"curated_matched_keywords"]].values.tolist()
+    year_keywords_df = df.groupby('year')['cites'].apply(list).reset_index()
+    year_keywords_list = year_keywords_df[['year',"cites"]].values.tolist()
     year_keywords_list.sort(key=lambda x: x[0])
 
     flatten = lambda l: [item for sublist in l for item in sublist]
 
-    all_words_list = []
+    """all_words_list = [] # in case want to do tfidf
     for row in year_keywords_list:
         keywords = flatten([el[2:-2].split("', '" ) for el in row[1]])
         all_words_list.extend(keywords)
-    total_word_counts_dict = Counter(all_words_list)
+    total_word_counts_dict = Counter(all_words_list)"""
 
-    year_to_fashion_terms_list_dict = {}
+    year_to_cities_list_dict = {}
     my_str = "yearly_fashion_terms:["
     for row in year_keywords_list:
         year = row[0]
         keywords = flatten([el[2:-2].split("', '" ) for el in row[1] if el != "Style"])
-        cnts = [[tup[0],tup[1]/total_word_counts_dict[tup[0]]] for tup in Counter(keywords).most_common(50)]
+        cnts = [[tup[0],tup[1]] for tup in Counter(keywords).most_common(50)]
         my_str += "[%s, %s],\n"%(year,cnts)
-        year_to_fashion_terms_list_dict[int(year)] = keywords
+        year_to_cities_list_dict[int(year)] = keywords
 
     my_str = my_str[:-2]+"],\n"
-    text_file = open("%s/data/react-codes/react_key_fashion_terms_by_date.txt"%DATA_PATH, "w")
+    text_file = open("%s/data/react-codes/react_locations_by_date.txt"%DATA_PATH, "w")
     text_file.write(my_str)
     text_file.close()
 
-    pickle.dump(year_to_fashion_terms_list_dict,open("%s/data/year_to_fashion_terms_list_dict.p"%DATA_PATH,"wb"))
+    pickle.dump(year_to_cities_list_dict,open("%s/data/year_to_cities_list_dict.p"%DATA_PATH,"wb"))
 
 
 # make map from terms to locations
 # add to table
+
+make_map_from_years_to_locations()
