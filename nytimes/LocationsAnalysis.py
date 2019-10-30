70bc19603911166d6ae6fae540cf272c6855b182
angwhen <angelmwen@gmail.com>
2019-10-29 20:37:46 -0400

    default message

--- nytimes/LocationsAnalysis.py
+++ nytimes/LocationsAnalysis.py
@@ -41,8 +41,8 @@ def make_map_from_years_to_locations():
     my_str = "yearly_locations:["
     for row in year_keywords_list:
         year = row[0]
-        keywords = flatten([el[2:-2].split("', '" ) for el in row[1] if el != ""])
-        cnts = [[tup[0],tup[1]] for tup in Counter(keywords).most_common(50)]
+        keywords = flatten([el[2:-2].split("', '" ) for el in row[1]])
+        cnts = [[tup[0],tup[1]] for tup in Counter(keywords).most_common(50) if tup[0] != ""]
         my_str += "[%s, %s],\n"%(year,cnts)
         year_to_cities_list_dict[int(year)] = keywords
 
