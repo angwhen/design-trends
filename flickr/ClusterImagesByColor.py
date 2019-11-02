523d409ce0b0f40c0110b322615355961ca6b86a
angwhen <angelmwen@gmail.com>
2019-11-02 00:26:46 -0400

    default message

--- flickr/ClusterImagesByColor.py
+++ flickr/ClusterImagesByColor.py
@@ -12,6 +12,7 @@ from time import time
 import cv2
 import random
 from collections import Counter
+from functools import reduce
 
 DATA_PATH = "."
 try:
@@ -20,9 +21,9 @@ try:
 except:
     print ("data is right here")
 
-def get_pixels_in_file(fname_num,every_few = 10):
+def get_pixels_in_file(fnum,every_few = 10):
     try:
-        res = pickle.load(open("%s/data/images/mask_rcnn_results/res_%d.p"%(DATA_PATH,fname_num),"rb"))
+        res = pickle.load(open("%s/data/images/mask_rcnn_results/res_%d.p"%(DATA_PATH,fnum),"rb"))
     except:
         return []
     masks = res[1]
@@ -30,17 +31,16 @@ def get_pixels_in_file(fname_num,every_few = 10):
 
     people_indices = []
     for i in range(0,masks.shape[0]): #the masks we have for people
-        if ids[i] == 0:
+        if ids[i] == 0: # 0 means person
             people_indices.append(i)
-
     if len(people_indices) == 0:
         return []
-    im = cv2.imread("%s/data/images/smaller_images/%d.jpg"%(DATA_PATH,fname_num))
+
+    im = cv2.imread("%s/data/images/smaller_images/%d.jpg"%(DATA_PATH,fnum))
     if (im.shape[0] != masks.shape[1] or im.shape[1] != masks.shape[2]):
         print ("some dimensional problem")
         return []
 
-    #print (fname_num)
     my_pixels = []
     not_greys = 0
     for ind in people_indices:
@@ -56,93 +56,80 @@ def get_pixels_in_file(fname_num,every_few = 10):
         return []
     return shuffle(my_pixels, random_state=0)[:max(36000,int(len(my_pixels)/every_few))] #dont let any image return too many pixels
 
-def make_clusters(num_clusters=7):
-    n_colors = 7
+class QuantizedImageBreakdown():
+    def __init__(self,colors_definitions,,fnum_to_counts_of_each_color_in_image_dict):
+        self.colors_definitions = colors_definitions
+        self.fnum_to_counts_of_each_color_in_image_dict = fnum_to_counts_of_each_color_in_image_dict
+    def get_fnums_to_colors_proportions_dict(self):
+        fnums_to_colors_proportions_dict = {}
+        return {}
 
-    df =  pd.read_csv("%s/data/url_title_and_file_data.csv"%DATA_PATH)
-    fnames_list = df[["file_name"]].values.tolist()
+def make_clusters(num_quantized_colors = 5, num_clusters = 7):
+    fnums_list = pickle.load(open("%s/data/basics/fnums_list.p"%DATA_PATH,"rb"))
 
     all_colors = []
-    palettes = {}
-    for fname in fnames_list:
-        fname_num = fname[0].split("/")[-1]
-        fname_num = (int) (fname_num.split(".jpg")[0])
-        all_pixels_curr = get_pixels_in_file(fname_num)
+    fnum_to_pixels_dict = {}
+    for fnum in fnums_list[:10]:
+        all_pixels_curr = get_pixels_in_file(fnum)
         all_colors.extend(all_pixels_curr)
         if len(all_pixels_curr) != 0:
-            palettes[fname_num] = all_pixels_curr
-    all_colors = np.array(all_colors)
-    image_array = all_colors
+            fnum_to_pixels_dict[fnum] = all_pixels_curr
+    all_colors_array_sample = shuffle(np.array(all_colors), random_state=0)[:500000]
 
-    print("Fitting model on a small sub-sample of the data")
-    t0 = time()
-    image_array_sample = shuffle(image_array, random_state=0)[:500000]
-    kmeans = KMeans(n_clusters=n_colors, max_iter=100,random_state=0).fit(image_array_sample)
-    print("done in %0.3fs." % (time() - t0))
+    print ("Quantization")
+    kmeans = KMeans(n_clusters=num_quantized_colors, max_iter=100,random_state=0).fit(all_colors_array_sample)
+    # Quantize each image
+    fnum_to_counts_of_each_color_in_image_dict= {}
+    for fnum in fnums_list:
+        if fnum in fnum_to_pixels_dict:
+            list_of_colors =  kmeans.predict(fnum_to_pixels_dict[fnum])
+            counts_of_each_color_in_image = [0]*n_colors
+            for color_ind in list_of_colors:
+                counts_of_each_color_in_image[color_ind] +=1
+            fnum_to_counts_of_each_color_in_image_dict[fnum] = counts_of_each_color_in_image
 
-    # Get labels for all points
-    print("Predicting color indices on each image")
-    fname_nums_in_order_list = []
-    color_labels_one_hots= []
-    for fname in fnames_list:
-        fname_num = fname[0].split("/")[-1]
-        fname_num = (int) (fname_num.split(".jpg")[0])
-        if fname_num in palettes:
-            list_ver =  kmeans.predict(palettes[fname_num])
-            one_hot_ver = [0]*n_colors
-            for el in list_ver:
-                one_hot_ver[el] +=1
-            fname_nums_in_order_list.append(fname_num)
-            color_labels_one_hots.append(one_hot_ver)
+    centroids = kmeans.cluster_centers_
+    print (centroids)
+    return
+    quantized_images_breakdown = QuantizedImageBreakdown(centroids,fnum_to_counts_of_each_color_in_image_dict)
+    pickle.dump(quantized_images_breakdown,open("%s/data/quantized_images_breakdown.p","wb"))
 
-    pickle.dump(color_labels_one_hots,open("%s/data/per_image_color_labels_one_hots.p"%DATA_PATH,"wb"))
+    # Clustering images
     print ("Clustering on those counts per color labels")
+    kmeans = KMeans(n_clusters=num_clusters,random_state=0).fit(fnum_to_counts_of_each_color_in_image_dict)
+    fnums_in_order_list = list(fnum_to_counts_of_each_color_in_image_dict.keys())
+    counts_of_each_color_in_image_list = [fnum_to_counts_of_each_color_in_image_dict[fnum] for fnum in fnums_in_order_list]
+    clusters = kmeans.predict(counts_of_each_color_in_image_list)
+    fnum_to_cluster_dict, cluster_to_fnums_dict = {}, {}
+    for i in range(0,len(fnums_in_order_list)):
+        fnum_to_cluster_dict[fnums_in_order_list[i]] = clusters[i]
+        if clusters[i] not in cluster_to_fnums_dict:
+            cluster_to_fnums_dict[clusters[i]] = []
+        cluster_to_fnums_dict[clusters[i]].append(fnums_in_order_list[i])
+    pickle.dump(fnum_to_cluster_dict,open("%s/data/fnum_to_cluster_number_dict.p"%DATA_PATH,"wb"))
+    pickle.dump(cluster_to_fnums_dict,open("%s/data/cluster_number_to_fnum_dict.p"%DATA_PATH,"wb"))
 
-    # random categorical data
-    #data = np.random.choice(20, (100, 10))
-    #print (data)
-    kmeans = KMeans(n_clusters=num_clusters,random_state=0).fit(color_labels_one_hots)
+    make_dict_of_cluster_and_year()
 
-    clusters = kmeans.predict(color_labels_one_hots)
-    fname_to_cluster_dict = {}
-    cluster_to_fnames_dict = {}
-    for i in range(0,len(fname_nums_in_order_list)):
-        fname_to_cluster_dict[fname_nums_in_order_list[i]] = clusters[i]
-        if clusters[i] not in cluster_to_fnames_dict:
-            cluster_to_fnames_dict[clusters[i]] = []
-        cluster_to_fnames_dict[clusters[i]].append(fname_nums_in_order_list[i])
-    pickle.dump(fname_to_cluster_dict,open("%s/data/file_num_to_cluster_number_dict.p"%DATA_PATH,"wb"))
-    pickle.dump(cluster_to_fnames_dict,open("%s/data/cluster_number_to_file_num_dict.p"%DATA_PATH,"wb"))
-    # Print the cluster centroids
-    #print(km.cluster_centroids_)
+def make_dict_of_cluster_and_year():
+    fnum_to_cluster_dict = pickle.load(open("%s/data/fnum_to_cluster_number_dict.p"%DATA_PATH,"rb"))
+    cluster_to_fnums_dict = pickle.load(open("%s/data/cluster_number_to_fnum_dict.p"%DATA_PATH,"rb"))
+    fnums_to_year_dict = pickle.load(open("%s/data/basics/fnum_to_year_dict.p"%DATA_PATH,"rb"))
 
-def make_dict_of_cluster_to_year():
-    fname_to_cluster_dict = pickle.load(open("%s/data/file_num_to_cluster_number_dict.p"%DATA_PATH,"rb"))
-    cluster_to_fnames_dict = pickle.load(open("%s/data/cluster_number_to_file_num_dict.p"%DATA_PATH,"rb"))
-
-    df =  pd.read_csv("%s/data/url_title_and_file_data.csv"%DATA_PATH)
-    year_and_fname = df[["year","file_name"]].values.tolist()
-    fname_nums_to_year_dict = {}
-    for el in  year_and_fname:
-        year = int(el[0])
-        fname_num = el[1].split("/")[-1]
-        fname_num = (int) (fname_num.split(".jpg")[0])
-        fname_nums_to_year_dict[fname_num] = year
-
+    # Find all years associated with each color cluster
     color_cluster_to_years_dict = {}
     for cc in cluster_to_fnames_dict.keys():
-        fname_num_list = cluster_to_fnames_dict[cc]
-        color_cluster_to_years_dict[cc] = [fname_nums_to_year_dict[fn] for fn in fname_num_list]
+        fnum_list = cluster_to_fnames_dict[cc]
+        color_cluster_to_years_dict[cc] = [fnums_to_year_dict[fn] for fn in fnum_list]
     pickle.dump(color_cluster_to_years_dict,open("%s/data/color_cluster_number_to_years_dict.p"%DATA_PATH,"wb"))
 
+    # Find most common color cluster for a given year
     years_to_most_common_cluster_dict = {}
-    for el in year_and_fname:
-        year = int(el[0])
-        fname_num = el[1].split("/")[-1]
-        fname_num = (int) (fname_num.split(".jpg")[0])
-        if fname_num not in fname_to_cluster_dict:
+    for fnum in fnums_to_year_dict.keys():
+        year = fnums_to_year_dict[fnum]
+        if fnum not in fnum_to_cluster_dict:
             continue
-        curr_cluster = fname_to_cluster_dict[fname_num]
+        curr_cluster = fnum_to_cluster_dict[fnum]
         if year not in years_to_most_common_cluster_dict:
             years_to_most_common_cluster_dict[year] = []
         years_to_most_common_cluster_dict[year].append(curr_cluster)
@@ -155,58 +142,43 @@ def rgb_list_to_hex_list(rgb_list):
     return ["#%02x%02x%02x"%(c[0],c[1],c[2]) for c in rgb_list]
 
 def make_react_codes(num_clusters=7):
-    """df =  pd.read_csv("%s/data/url_title_and_file_data.csv"%DATA_PATH)
-    my_urls_list = df[["url","year","file_name"]].values.tolist()
-    # Make filename num to url dict
-    fnum_to_url_dict = {}
-    for el in my_urls_list:
-        url = el[0]
-        p1 = url.split("_o")[0]
-        url = p1+"_n.jpg"
-        fname_num = el[2].split("/")[-1]
-        fname_num = (int) (fname_num.split(".jpg")[0])
-        fnum_to_url_dict[fname_num]=url"""
-
     fnum_to_url_dict = pickle.load(open("%s/data/basics/fnum_to_flickr_url_dict.p"%DATA_PATH,"rb"))
     cluster_to_fnums_dict = pickle.load(open("%s/data/cluster_number_to_file_num_dict.p"%DATA_PATH,"rb"))
     for i in range(0,num_clusters):
         random.shuffle(cluster_to_fnums_dict[i])
-        
-    min_len = len(cluster_to_fnums_dict[0])
-    for k in range(1,num_clusters):
-        min_len = min(len(cluster_to_fnums_dict[k]),min_len)
-
+    min_len = reduce((lambda x, y: max(x,y)), [len(fnums) for fnums in cluster_to_fnums_dict.values()])
 
     # IMAGES
     my_str = "images: [\n"
     for i in range(0, min_len):
         my_str += "["
         for k in range(0,num_clusters):
-            curr_fname = cluster_to_fnums_dict[k][i]
-            my_str += "'%s',"%fnum_to_url_dict[curr_fname]
+            curr_fnum = cluster_to_fnums_dict[k][i]
+            my_str += "'%s',"%fnum_to_url_dict[curr_fnum]
         my_str = my_str[:-1]+"],\n"
     my_str = my_str[:-2]+"\n],\n"
 
-    # COLORS
+    # DOM COLORS
     fnum_to_palettes_dict = pickle.load(open("%s/data/5_color_fnum_to_palettes_dict.p"%DATA_PATH,"rb"))
     my_str += "dom_colors: [\n"
     for i in range(0, min_len):
         my_str += "["
         for k in range(0,num_clusters):
-            curr_fname = cluster_to_fnums_dict[k][i]
-            if curr_fname in fnum_to_palettes_dict:
-                my_str += "%s,"%rgb_list_to_hex_list(fnum_to_palettes_dict[curr_fname])
+            curr_fnum = cluster_to_fnums_dict[k][i]
+            if curr_fnum in fnum_to_palettes_dict:
+                my_str += "%s,"%rgb_list_to_hex_list(fnum_to_palettes_dict[curr_fnum])
             else:
                 my_str += "[],"
         my_str = my_str[:-1]+"],\n"
     my_str = my_str[:-2]+"\n],\n"
+
+    # QUANTIZED COLORS: each one gets a dict from hex color to proportion
 
     text_file = open("%s/data/react-codes/react_color_clustering_page_codes.txt"%DATA_PATH, "w")
     text_file.write(my_str)
     text_file.close()
     print ("Done")
 
-
-#make_clusters(num_clusters=7)
-#make_dict_of_cluster_to_year(num_clusters=7)
-make_react_codes()
+num_clusters = 7
+make_clusters(num_clusters=num_clusters)
+make_react_codes(num_clusters=num_clusters)
