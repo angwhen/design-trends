76da1aeaa34537d971985b9632b9dda2c3cca4eb
angwhen <angelmwen@gmail.com>
2019-11-01 21:36:36 -0400

    default message

--- flickr/MakeBasicHelpfulDicts.py
+++ flickr/MakeBasicHelpfulDicts.py
@@ -19,7 +19,7 @@ def make_fnum_to_flickr_url_dict(df):
         fname_num = el[1].split("/")[-1]
         fname_num = (int) (fname_num.split(".jpg")[0])
         fnum_to_url_dict[fname_num]=url
-    pickle.dump(fnum_to_url_dict,open("%s/fnum_to_flickr_url_dict.p"%DATA_PATH,"wb"))
+    pickle.dump(fnum_to_url_dict,open("%s/data/basics/fnum_to_flickr_url_dict.p"%DATA_PATH,"wb"))
 
 def make_fnum_to_year_dict(df):
     years_list = df[["file_name","year"]].values.tolist()
@@ -28,7 +28,7 @@ def make_fnum_to_year_dict(df):
         fname_num = int(el[0].split(".")[0].split("/")[-1])
         year = int(el[1])
         fnum_to_year_dict[fname_num] = year
-    pickle.dump(fnum_to_year_dict,open("%s/fnum_to_year_dict.p"%DATA_PATH,"wb"))
+    pickle.dump(fnum_to_year_dict,open("%s/data/basics/fnum_to_year_dict.p"%DATA_PATH,"wb"))
 
 def make_fnums_list(df):
     my_list = df[["file_name"]].values.tolist()
@@ -36,9 +36,9 @@ def make_fnums_list(df):
     for el in my_list:
         fname_num = int(el[0].split(".")[0].split("/")[-1])
         fnums_list.append(fname_num)
-    pickle.dump(fnums_list,open("%s/fnums_list.p"%DATA_PATH,"wb"))
+    pickle.dump(fnums_list,open("%s/data/basics/fnums_list.p"%DATA_PATH,"wb"))
 
-df =  pd.read_csv("%s/data/url_title_and_file_data.csv"%DATA_PATH)
+df =  pd.read_csv("%s/data/basics/url_title_and_file_data.csv"%DATA_PATH)
 make_fnum_to_flickr_url_dict(df)
 make_fnum_to_year_dict(df)
 make_fnums_list(df)
