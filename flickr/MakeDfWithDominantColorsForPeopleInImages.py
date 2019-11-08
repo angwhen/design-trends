294a67f6e96fd81e59a14b80d0d7fca997d468d2
angwhen <angelmwen@gmail.com>
2019-11-07 20:06:43 -0500

    default message

--- flickr/MakeDfWithDominantColorsForPeopleInImages.py
+++ flickr/MakeDfWithDominantColorsForPeopleInImages.py
@@ -15,46 +15,6 @@ try:
 except:
     print ("data is right here")
 
-def get_pixels_in_fnums(fnums,sample_amount):
-    all_pixels = []
-    for fnum in fnums:
-        try:
-            res = pickle.load(open("%s/data/images/mask_rcnn_results/res_%d.p"%(DATA_PATH,fnum),"rb"))
-        except:
-            continue
-        masks, ids = res[1], res[2]
-        people_indices = [i for i in range(0,masks.shape[0]) if ids[i] == 0]
-        if len(people_indices) == 0:
-            continue
-
-        im = cv2.imread("%s/data/images/smaller_images/%d.jpg"%(DATA_PATH,fnum))
-        if (im.shape[0] != masks.shape[1] or im.shape[1] != masks.shape[2]):
-            print ("Dimensional problem on %d, image:%d, %d vs masks: %d, %d"%(fnum, im.shape[0],im.shape[1],masks.shape[1],masks.shape[2]))
-            continue
-
-        sum_mask = masks[people_indices[0]]
-        for ind in people_indices[1:]:
-            sum_mask += masks[ind]
-        my_pixels = im[sum_mask!=0]
-        all_pixels.extend(shuffle(my_pixels, random_state=0)[:int(len(my_pixels)/sample_amount)])
-
-    return shuffle(all_pixels,random_state=0)[:360000]
-
-def make_yearly_color_palettes(num_colors=10,sample_amount=5):
-    year_to_fnums_dict=pickle.load(open("%s/data/basics/year_to_fnums_dict.p"%DATA_PATH,"rb"))
-    year_to_color_palettes_dict = {}
-    for year in year_to_fnums_dict.keys():
-        if year in year_to_color_palettes_dict:
-            continue
-        year_pixels = get_pixels_in_fnums(year_to_fnums_dict[year],sample_amount)
-        if len(year_pixels) == 0:
-            print ("year %d has no valid pixels to use"%year)
-            continue
-        ct = ColorThief(year_pixels)
-        color_list = ct.get_palette(color_count=num_colors)
-        year_to_color_palettes_dict[year] = color_list
-    pickle.dump(year_to_color_palettes_dict,open("%s/data/year_to_%d_color_palettes_dict.p"%(DATA_PATH,num_colors),"wb"))
-
 def make_color_palettes(num_colors=10,output_fname="color_palettes",sample_amount=1):
     df =  pd.read_csv("%s/data/url_title_and_file_data.csv"%DATA_PATH)
     fnames_list = df[["file_name"]].values.tolist()
@@ -208,26 +168,5 @@ def make_colors_list_for_react():
     text_file.write(my_str)
     text_file.close()
 
-def make_yearly_colors_list_for_react(num_colors=10):
-    year_to_color_palettes_dict=pickle.load(open("%s/data/year_to_%d_color_palettes_dict.p"%(DATA_PATH,num_colors),"rb"))
-
-    my_str = "yearly_colors:["
-    for curr_year in sorted(list(year_to_color_palettes_dict.keys())):
-        curr_colors =  sort_colors_lists([year_to_color_palettes_dict[curr_year]])[0]
-        my_str += "["
-        for i in range(0,len(curr_colors)):
-            c = curr_colors[i]
-            my_str+="'#%02x%02x%02x'," %(c[0],c[1],c[2])
-        my_str +="%d"%curr_year
-        my_str += "],\n"
-    my_str = my_str[:-2] + "],"
-
-    text_file = open("%s/data/react-codes/react_yearly_colors_for_colors_slides.txt"%DATA_PATH, "w")
-    text_file.write(my_str)
-    text_file.close()
-    print ("Done making yearly colors react codes")
-
 #make_color_palettes(num_colors=5,output_fname="5_color_fnum_to_palettes_dict")
 #make_colors_list_for_react()
-#make_yearly_color_palettes(num_colors=10)
-make_yearly_colors_list_for_react(num_colors=10)
