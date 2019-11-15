e2f4433b07fb1878e671882af0e1650cce4cf5ed
angwhen <angelmwen@gmail.com>
2019-11-14 22:29:18 -0500

    default message

--- flickr/ColorsTogether.py
+++ flickr/ColorsTogether.py
@@ -7,7 +7,7 @@ from time import time
 from collections import Counter
 from functools import reduce
 import pickle, math, random, cv2, colorsys
-import HSVHelpers, GetPixelsHelpers, YearlyColorGrids, CLusterImagesByColor
+import HSVHelpers, GetPixelsHelpers, YearlyColorGrids, ClusterImagesByColor
 
 CLusterImagesByColor.make_color_clusters(Q=5, K=7, color_rep="hsv",remove_monochrome=True, remove_predom_faces = False, remove_skin=False)
 YearlyColorGrids.yearly_grids(num_dom_colors=10, Q= 25, color_rep="hsv",remove_monochrome=True, remove_predom_faces = False, remove_skin=False)
